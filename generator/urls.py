7bca06684e8c21a8278fb14063bb9ecd13a265b0
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-13 17:46:31 +0300

    Add base.html and download cv button

--- generator/urls.py
+++ generator/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pdf.views import accept, resume
+from pdf.views import accept, resume, list
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",accept,name='accept'),
     path('<int:id>/',resume,name='resume'),
+    path('list/', list,name='list'),
 ]
